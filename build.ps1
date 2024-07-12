65382b108906358efb64ceb5d7a9a8cb4ade5ecc
otavepto <153766569+otavepto@users.noreply.github.com>
2024-07-12 21:20:38 +0300

    simplify build scripts + add release action

--- build.ps1
+++ build.ps1
@@ -1,107 +1,74 @@
 param(
-	[ValidateSet("all", "netframework", "net6.0-win32", "net6.0-win64", "net6.0-win-arm", "net6.0-win-arm64", "net6.0-linux64", "net6.0-linux-arm", "net6.0-linux-arm64", "net6.0-osx64" )]
-	[string]$framework = 'all',
-	${-no-msbuild}
+  [ValidateSet(
+    "netframework",
+    "win-x86", "win-x64", "win-arm64",
+    "linux-x64", "linux-arm", "linux-arm64",
+    "osx-x64"
+  )]
+  [string]$framework = 'netframework',
+  ${-no-msbuild}
 )
 
 $ErrorActionPreference = 'Stop'
 
 $configuration = 'Release'
+# https://learn.microsoft.com/en-us/dotnet/standard/frameworks#latest-versions
+$tfm = "net8.0"
 
 function BuildNETFramework {
-	Write-Host 'Building .NET Framework x86 and x64 binaries'
-	if (${-no-msbuild}) {
-		dotnet build -v:m -c $configuration
-		if ($LASTEXITCODE) { 
-			Write-Host
-			Write-Host ==========================
-			Write-Host "THE BUILD OPERATION ENCOUNTERED AN ERROR. EXIT CODE: $LASTEXITCODE" -ForegroundColor Red
-			exit $LASTEXITCODE 
-		}
-	}
-	else {
-		msbuild -v:m -m -restore -t:Build -p:Configuration=$configuration
-		if ($LASTEXITCODE) { 
-			Write-Host
-			Write-Host ==========================
-			Write-Host "THE BUILD OPERATION ENCOUNTERED AN ERROR. EXIT CODE: $LASTEXITCODE" -ForegroundColor Red
-			exit $LASTEXITCODE 
-		}
-	}
+  Write-Host 'Building .NET Framework x86 and x64 binaries'
+  if (${-no-msbuild}) {
+    dotnet build -o "bin\$configuration\netframework\" -v:m -c $configuration -p:TargetFrameworks=net481
+    if ($LASTEXITCODE) { 
+      Write-Host
+      Write-Host ==========================
+      Write-Host "THE BUILD OPERATION ENCOUNTERED AN ERROR. EXIT CODE: $LASTEXITCODE" -ForegroundColor Red
+      exit $LASTEXITCODE 
+    }
+  }
+  else {
+    msbuild -p:OutputPath="..\bin\$configuration\netframework\" -v:m -m -restore -t:Build -p:Configuration=$configuration -p:TargetFrameworks=net481
+    if ($LASTEXITCODE) { 
+      Write-Host
+      Write-Host ==========================
+      Write-Host "THE BUILD OPERATION ENCOUNTERED AN ERROR. EXIT CODE: $LASTEXITCODE" -ForegroundColor Red
+      exit $LASTEXITCODE 
+    }
+  }
 }
 
 function BuildNETCore {
-	param([string]$architecture, [string]$tfm)
+  param([string]$runtimeidentifier)
 
-	Write-Host "Building .NET $architecture binaries"
+  Write-Host "Building .NET binaries, arch=$runtimeidentifier tfm=$tfm"
 
-	$runtimeidentifier = "$architecture"
-
-	if (${-no-msbuild}) {
-		dotnet publish NETReactorSlayer.CLI\NETReactorSlayer.CLI.csproj -v:m -c $configuration -f $tfm -r $runtimeidentifier --self-contained -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishTrimmed=True -p:PublishSingleFile=true
-		if ($LASTEXITCODE) { 
-			Write-Host
-			Write-Host ==========================
-			Write-Host "THE BUILD OPERATION ENCOUNTERED AN ERROR. EXIT CODE: $LASTEXITCODE" -ForegroundColor Red
-			exit $LASTEXITCODE 
-		}
-	}
-	else {
-		msbuild NETReactorSlayer.CLI\NETReactorSlayer.CLI.csproj -v:m -m -restore -t:Publish -p:Configuration=$configuration -p:TargetFramework=$tfm -p:RuntimeIdentifier=$runtimeidentifier -p:SelfContained=True -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishTrimmed=True -p:PublishSingleFile=true
-		if ($LASTEXITCODE) { 
-			Write-Host
-			Write-Host ==========================
-			Write-Host "THE BUILD OPERATION ENCOUNTERED AN ERROR. EXIT CODE: $LASTEXITCODE" -ForegroundColor Red
-			exit $LASTEXITCODE
-		 }
-	}
+  if (${-no-msbuild}) {
+    dotnet publish -o "bin\$configuration\$runtimeidentifier\" NETReactorSlayer.CLI\NETReactorSlayer.CLI.csproj -v:m -c $configuration -f $tfm -r $runtimeidentifier --self-contained -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishTrimmed=True -p:PublishSingleFile=true -p:TargetFrameworks=$tfm
+    if ($LASTEXITCODE) { 
+      Write-Host
+      Write-Host ==========================
+      Write-Host "THE BUILD OPERATION ENCOUNTERED AN ERROR. EXIT CODE: $LASTEXITCODE" -ForegroundColor Red
+      exit $LASTEXITCODE 
+    }
+  }
+  else {
+    msbuild -p:OutputPath="..\bin\$configuration\$runtimeidentifier\" NETReactorSlayer.CLI\NETReactorSlayer.CLI.csproj -v:m -m -restore -t:Publish -p:Configuration=$configuration -p:TargetFramework=$tfm -p:RuntimeIdentifier=$runtimeidentifier -p:SelfContained=True -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishTrimmed=True -p:PublishSingleFile=true -p:TargetFrameworks=$tfm
+    if ($LASTEXITCODE) { 
+      Write-Host
+      Write-Host ==========================
+      Write-Host "THE BUILD OPERATION ENCOUNTERED AN ERROR. EXIT CODE: $LASTEXITCODE" -ForegroundColor Red
+      exit $LASTEXITCODE
+     }
+  }
 }
 
-if($framework -eq 'all') {
-	BuildNETFramework
-	BuildNETCore win-x86 "net6.0"
-	BuildNETCore win-x64 "net6.0"
-	BuildNETCore win-arm "net6.0"
-	BuildNETCore win-arm64 "net6.0"
-	BuildNETCore linux-x64 "net6.0"
-	BuildNETCore linux-arm "net6.0"
-	BuildNETCore linux-arm64 "net6.0"
-	BuildNETCore osx-x64 "net6.0"
 
-	Write-Host
-	Write-Host ==========================
-	Write-Host "BUILD OPERATION COMPLETED SUCCESSFULLY" -ForegroundColor Green
+if(($framework -eq 'netframework') -or ($framework -eq 'all')){
+  BuildNETFramework
+} else {
+  BuildNETCore $framework
 }
-else {
-	if($framework -eq 'netframework'){
-		BuildNETFramework
-	}
-	elseif($framework -eq 'net6.0-win32'){
-		BuildNETCore win-x86 'net6.0'
-	}
-	elseif($framework -eq 'net6.0-win64'){
-		BuildNETCore win-x64 'net6.0'
-	}
-	elseif($framework -eq 'net6.0-win-arm'){
-		BuildNETCore win-arm 'net6.0'
-	}
-	elseif($framework -eq 'net6.0-win-arm64'){
-		BuildNETCore win-arm64 'net6.0'
-	}
-	elseif($framework -eq 'net6.0-linux64'){
-		BuildNETCore linux-x64 'net6.0'
-	}
-	elseif($framework -eq 'net6.0-linux-arm'){
-		BuildNETCore linux-arm 'net6.0'
-	}
-	elseif($framework -eq 'net6.0-linux-arm64'){
-		BuildNETCore linux-arm64 'net6.0'
-	}
-	elseif($framework -eq 'net6.0-osx64'){
-		BuildNETCore osx-x64 'net6.0'
-	}
 
-	Write-Host
-	Write-Host ==========================
-	Write-Host "BUILD OPERATION COMPLETED SUCCESSFULLY" -ForegroundColor Green
-}
+Write-Host
+Write-Host ==========================
+Write-Host "Done"
